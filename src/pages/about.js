ddc82cecbd78dc2f0aafc6075d69ca7693ff67a9
Alexander Bandukwala <7h3kk1d@gmail.com>
2022-09-20 20:20:49 -0500

    Fix duplication

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,6 @@ export default () =>
       <p>
         {"Software Engineer interested in building the future of computing. "}
         {"Particularly interested in utilizing the intersection of Programming Language Theory and Human-Computer Interaction to make the power of computing better for everyone."}
-        {"Particularly interested in utilizing the intersection of Programming Language Theory and Human-Computer Interaction to make the power of computing better for everyone."}
       </p>
     </div>
   </Layout>
