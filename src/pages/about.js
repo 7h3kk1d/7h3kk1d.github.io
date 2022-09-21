ea3c1ac12049c16730b2ea14ad96f410400557a3
Alexander Bandukwala <7h3kk1d@gmail.com>
2022-09-20 20:20:08 -0500

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,9 @@ export default () =>
         About Me
     </h1>
       <p>
-        {"Aspiring academic/software engineer interested in PL, distributed systems and HCI"}
+        {"Software Engineer interested in building the future of computing. "}
+        {"Particularly interested in utilizing the intersection of Programming Language Theory and Human-Computer Interaction to make the power of computing better for everyone."}
+        {"Particularly interested in utilizing the intersection of Programming Language Theory and Human-Computer Interaction to make the power of computing better for everyone."}
       </p>
     </div>
   </Layout>
