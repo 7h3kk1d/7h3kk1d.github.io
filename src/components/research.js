f722c58379ad6af883d617f67f5596e21fe003ce
Alexander Bandukwala <7h3kk1d@gmail.com>
2024-10-22 14:36:20 -0400

    Fix link

--- src/components/research.js
+++ src/components/research.js
@@ -4,7 +4,7 @@ export const research =   <div>
   Current Research Project
 </h1>
 <p>
-  I am currently looking at adding features to Hazel to make it amenable as a <a>live, rich, composable, and collaborative planetary compute engine</a>.
+  I am currently looking at adding features to Hazel to make it amenable as a <a href="https://hazel.org/papers/propl24.pdf">live, rich, composable, and collaborative planetary compute engine</a>.
   The goal is to help address the climate crisis by creating better tools for climate scientists to interface with data and enabling them to better collaborate
   and share their work. This is currently taking the form of providing more data-oriented features to Hazel, such as a more robust data model and better support
   for tabular data.
