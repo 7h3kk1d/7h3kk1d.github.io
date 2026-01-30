7b9f00de97a9b7c38f3ff18f73d6f0e81d6aa52b
Alexander Bandukwala <alexander@bandukwala.me>
2026-01-30 13:15:54 -0500

    Dark mode

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,35 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+const React = require('react')
+
+// Prevent flash of default theme on page load
+exports.onRenderBody = ({ setPreBodyComponents }) => {
+  const themeInitScript = `
+    (function() {
+      function getInitialTheme() {
+        const persistedTheme = window.localStorage.getItem('theme');
+        const hasPersistedTheme = typeof persistedTheme === 'string';
+        
+        if (hasPersistedTheme) {
+          return persistedTheme;
+        }
+        
+        const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        return systemPrefersDark ? 'dark' : 'light';
+      }
+      
+      const theme = getInitialTheme();
+      document.documentElement.setAttribute('data-theme', theme);
+    })();
+  `
+
+  setPreBodyComponents([
+    React.createElement('script', {
+      key: 'theme-init',
+      dangerouslySetInnerHTML: {
+        __html: themeInitScript,
+      },
+    }),
+  ])
+}
